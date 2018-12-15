c9368cca2f2dec24e58f27e62941fbdfebd605f7
chisom5 <okoyechisom54@gmail.com>
2018-12-15 11:52:30 +0100

    remove the Un-needed raiseTo function

--- js/calculator.js
+++ js/calculator.js
@@ -1,22 +1,22 @@
 var txtField = document.getElementById('show');
 
 // for adding keyboard functionality
-document.addEventListener('keypress',function(event){
-    if(event.defaultPrevented){
-    return;
+document.addEventListener('keypress', function(event) {
+    if (event.defaultPrevented) {
+        return;
     }
     var key = event.key || event.keyCode;
     calc(key);
-    }); 
+});
 
 function calc(key) {
-    var allowedValues = ['0','1','2','3','4','5','6','7','8','9','+','-','/','*','(',')','.'];
+    var allowedValues = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '/', '*', '(', ')', '.'];
     var txt;
-    if(key === 'Enter'){
+    if (key === 'Enter') {
         answer(txt);
         return;
     }
-    if(allowedValues.includes(key)){
+    if (allowedValues.includes(key)) {
         display(key);
     }
 }
@@ -48,13 +48,13 @@ function answer(txt) {
     txt = txtField.value;
     // if there is no input, this is to prevent undefined
     // from showing in the text field
-    if(txt != ""){
-    try {
-    txt = eval(txt);
-    txtField.value = txt;     
-    } catch (error) {
-     txtField.value = "Syntax error";   
-    }
+    if (txt != "") {
+        try {
+            txt = eval(txt);
+            txtField.value = txt;
+        } catch (error) {
+            txtField.value = "Syntax error";
+        }
     }
 }
 
@@ -79,9 +79,9 @@ function tan(txt) {
 
 function log(txt) {
     txt = txtField.value;
-    if(txt != ""){
-    txt = Math.log10(txt);
-    txtField.value = txt;
+    if (txt != "") {
+        txt = Math.log10(txt);
+        txtField.value = txt;
     }
 }
 
@@ -97,7 +97,7 @@ function percent(txt) {
     txtField.value = val * 0.01;
 }
 
-pie = 3.142;
+const pie = 3.142;
 
 function pier(txt) {
     txt = txtField.value;
@@ -111,10 +111,12 @@ function square(txt) {
     txtField.value = txt;
 }
 
-function raiseTo(base, power) {
-    base = txtField.value;
-    console.log(base);
-}
+// there is no need for a raiseTo function this since we are using ** to represent it.
+
+// function raiseTo(base, power) {
+//     base = txtField.value;
+//     console.log(base);
+// }
 
 function factorial(txt) {
     txt = txtField.value;
